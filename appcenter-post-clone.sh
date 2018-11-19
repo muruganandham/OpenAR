eafc1ca4fd39731cafbdfb7e1fa5baa9ff56f271
Muruganandham K <muruganandham23@gmail.com>
2018-11-19 19:49:44 +0530

    updates

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Example: Clone a required repository
-git clone https://github.com/example/SomeProject
+git clone https://github.com/muruganandham/OpenAR
 
 # Example: Install App Center CLI
 npm install -g appcenter-cli
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,5 +3,5 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
-plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
+plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/OpenAR/Info.plist
 fi

0d5e1ebd9f15c810c7442a3899f65768a8f4177f
Muruganandham K <muruganandham23@gmail.com>
2018-11-20 11:39:52 +0530

    Added Hockey SDK

--- OpenAR/AppDelegate.swift
+++ OpenAR/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import HockeySDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        BITHockeyManager.shared().configure(withIdentifier:"a08048d16b5044f0bebb6006a1d7af76")
+        BITHockeyManager.shared().start()
+        BITHockeyManager.shared().authenticator.authenticateInstallation()
+        
         return true
     }
 

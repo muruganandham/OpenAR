61405af2ff8caa1f80bf159ad1a61a3ff2088d55
Muruganandham K <muruganandham23@gmail.com>
2018-11-22 15:10:11 +0530

    edited sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" != "master" ];
-then
-plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
-fi
+pod install

ce6af621e1ae6e397a59f3a3b2da26ec5fb02ae9
Muruganandham K <muruganandham23@gmail.com>
2018-11-22 17:34:57 +0530

    added scripts

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,5 +3,10 @@
 # Example: Clone a required repository
 git clone https://github.com/muruganandham/OpenAR
 
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
+
+
 # Example: Install cocoapods
 pod install
